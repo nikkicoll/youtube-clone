d6b4da773e3bcc69a86c6365c15642a5a1173c18
Nikki <nikki@Nikkis-MacBook-Pro.local>
2017-01-30 15:35:11 -0800

    Refactor parent component to be class-based and make api call to YT

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -16,10 +16,23 @@ class SearchBar extends Component {
   }
   // Every class-based component MUST have a render method
   render() {
-    return <input onChange={this.onInputChange}/>
+    return (
+      <div>
+        Search:
+        <input
+          // This input value will only change if the state changes
+          value={this.state.term}
+          // Must bind 'this' to the fn because otherwise referring to wrong object
+          onChange={this.onInputChange.bind(this)}
+          placeholder='search'
+        />
+      </div>
+    );
   }
 
   onInputChange(event) {
+    // You only ever update state by using this.setState
+    // Whenever state is updated, the entire component gets re-rendered
     this.setState({ term: event.target.value});
   }
 }
@@ -30,3 +43,6 @@ export default SearchBar;
 // Handling events in React:
   // Event handler (function) for when event occurs
   // Pass the handler to the element you want it to monitor
+
+// Declarative syntax w/ React
+  // The value of the input depends on the state
--- src/index.js
+++ src/index.js
@@ -3,25 +3,45 @@
 // Render this component to the DOM
 
 // React to create and interact with components
-import React from 'react';
+import React, { Component } from 'react';
 // ReactDOM is it's own library to interact with the actual DOM
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search';
 import YouTubeAPISearch from 'youtube-api-search';
 
-
 const API_KEY = 'AIzaSyCBiRbyMJvxs4E-XkIaIUq9gWsNGdp35EQ';
+
+
 // Create a component class (a factory)
 // This is considered a 'dumb' functional component
-const App = () => {
-  return (
-    <div>
-      <h1>YouTube</h1>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    }
+    // Whenever the app starts up this constructor method will be invoked right away and we get the videos
+    // from YouTube right away
+    YouTubeAPISearch({ key: API_KEY, term: 'weight loss'}, (data) => {
+      this.setState({ videos: data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>YouTube</h1>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Render the INSTANCE of App component to be attached to the div with container class
 // As long as our JSX tags are empty between, can become self closing tag
 ReactDOM.render(<App />, document.querySelector('.container'));
+
+// React has downward data flow: the most parent app should handle passing data down
+// Here the App component is going to handle fetching the data from YouTube
+  // since all other components will use that info
